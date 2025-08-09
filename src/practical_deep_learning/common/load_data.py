e12589cedc6b59c141d4d9ea0cadda9014880e86
Chris Bayer <c4.bayer@gmail.com>
2025-08-09 08:33:09 +0200

    load data

--- src/practical_deep_learning/common/load_data.py
+++ src/practical_deep_learning/common/load_data.py
@@ -6,6 +6,7 @@ from fastcore.all import L
 
 
 def search_images(keywords: str, max_results: int = 200) -> L:
+    
     return L(DDGS().images(keywords, max_results=max_results)).itemgot("image")
 
 
