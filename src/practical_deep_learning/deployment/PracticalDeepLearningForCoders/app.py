a462dfca7f01e25b72e36d31e5d75b43c38ca3a3
Chris Bayer <c4.bayer@gmail.com>
2025-09-10 17:27:21 +0200

    fix deployment

--- src/practical_deep_learning/deployment/PracticalDeepLearningForCoders/app.py
+++ src/practical_deep_learning/deployment/PracticalDeepLearningForCoders/app.py
@@ -1,13 +1,20 @@
+from pathlib import Path
+
 import gradio as gr
 from fastai.vision.all import load_learner
 
-learner = load_learner("export.pkl")
+script_dir = Path(__file__).parent
+learner = load_learner(script_dir / "export.pkl")
 
 demo = gr.Interface(
     fn=lambda img: learner.predict(img)[0],
     inputs=gr.Image(),
     outputs=gr.Textbox(),
-    examples=["cubic.png", "quadratic.png", "linear.png"],
+    examples=[
+        str(script_dir / "cubic.png"),
+        str(script_dir / "quadratic.png"),
+        str(script_dir / "linear.png"),
+    ],
 )
 
 demo.launch()
